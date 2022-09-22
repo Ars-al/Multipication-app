781cf71886464671bce42fd705776e3725b001aa
Ars-al <113255844+Ars-al@users.noreply.github.com>
2022-09-22 15:00:01 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,34 @@
+const num1 = Math.ceil(Math.random() * 10);
+const num2 = Math.ceil(Math.random() * 10);
+
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score");
+
+let score = JSON.parse(localStorage.getItem("score"));
+
+if(!score) {
+    score = 0;
+}
+
+scoreEl.innerText = `SCORE: ${score}`;
+
+questionEl.innerText = `What is ${num1} multiply by ${num2}?`;
+
+const correctAns = num1 * num2;
+
+formEl.addEventListener("submit", () => {
+    const ans = +input.value;
+    if(ans === correctAns) {
+        score++;
+        updatelocalstorage();
+    }else {
+        score--;
+        updatelocalstorage();
+    }
+});
+
+function updatelocalstorage() {
+    localStorage.setItem("score", JSON.stringify(score));
+}
